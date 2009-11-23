81a4ce9f0ecebe3804709578cea76dec43c4e435
James Hensman <james.hensman@gmail.com>
2009-11-23 16:13:39 +0100

    modified:   PCA_EM.py bufixing and testing. particluarly for X2reconstructed

--- PCA_EM.py
+++ PCA_EM.py
@@ -78,69 +78,70 @@ class PCA_EM_missing:
 	def __init__(self,data,target_dim):
 		"""Maximum likelihood PCA by the EM algorithm, allows for missing data.  uses a masked array to 'hide' the elements of X that are NaN"""
 		self.X = np.array(data)
-		self.Xmasked = np.ma.masked_array(data,np.ma.make_mask(np.isnan(self.X)))
+		self.imask,self.jmask = np.nonzero(np.isnan(self.X))#positions that are missing.
+		self.indices = [np.nonzero(np.isnan(x)-1)[0] for x in self.X] #positions that are not missing...
 		self.N,self.d = self.X.shape
 		self.q = target_dim
 		
 	def learn(self,niters):
-		self.mu = np.asarray(self.Xmasked.mean(0)).reshape(self.d,1)#ML solution for mu
-		#use a masked array to decide which data we're learning from.
-		self.X2masked = self.Xmasked - self.mu.T
-		#make an unmasked copy of X (to be filled with the reconstructed values)
-		self.X2unmasked = np.array(self.Xmasked).copy()
-		self.imask,self.jmask = np.nonzero(np.isnan(self.X2unmasked))
-		self.X2unmasked[self.imask,self.jmask] = 0
+		self.Xreconstruct = self.X.copy()
+		self.Xreconstruct[self.imask,self.jmask] = 0
+		self.mu = np.sum(self.Xreconstruct,0)/(self.X.shape[0]-np.sum(np.isnan(self.X),0))
+		
+		self.X2 = self.X.copy()-self.mu
+		self.X2reconstruct = self.X.copy() - self.mu
 		#initialise paramters:
 		self.W = np.random.randn(self.d,self.q)
 		self.sigma2 = 1.2
 		#pre-allocate self.m_Z and self.S_Z 
-		self.m_Z = np.zeros((self.X2masked.shape[0],self.q))
-		self.S_Z = np.zeros((self.X2masked.shape[0],self.q,self.q))
+		self.m_Z = np.zeros((self.X2.shape[0],self.q))
+		self.S_Z = np.zeros((self.X2.shape[0],self.q,self.q))
 		for i in range(niters):
 			print i,self.sigma2
 			self.E_step()
 			self.M_step()
+		self.Xreconstruct = self.X2reconstruct + self.mu
 
 	def E_step(self):
 		""" This should handle missing data, but needs testing (TODO)"""
-		Ms = np.zeros((self.X2masked.shape[0],self.q,self.q)) #M is going to be different for (potentially) every data point
-		for m,x,i in zip(Ms,self.X2masked,np.arange(self.X2masked.shape[0])):
-			index = np.nonzero(x.mask-1)[0]#select non masked parts
-			W = self.W.take(index,0)# get relevant bits of W
-			x2 = np.array(x).take(index) # get relevant bits of x
+		Ms = np.zeros((self.X.shape[0],self.q,self.q)) #M is going to be different for (potentially) every data point
+		for m,x,i,mz,sz in zip(Ms,self.X2,self.indices,self.m_Z,self.S_Z):
+			W = self.W.take(i,0)# get relevant bits of W
+			x2 = np.array(x).take(i) # get relevant bits of x
 			m[:,:] = np.dot(W.T,W) + np.eye(self.q)*self.sigma2
 			mchol = linalg.cholesky(m)
 			minv = linalg.cho_solve((mchol,1),np.eye(self.q))
-			self.m_Z[i,:] = linalg.cho_solve((mchol,1),np.dot(W.T,x2.reshape(index.size,1))).T
-			self.S_Z[i,:,:] = minv*self.sigma2
+			mz[:] = linalg.cho_solve((mchol,1),np.dot(W.T,x2.reshape(i.size,1))).T
+			sz[:,:] = minv*self.sigma2
 			
 		#calculate reconstructed X values
-		Xreconstruct = np.dot(self.m_Z,self.W.T)
-		self.X2unmasked[self.imask,self.jmask] = Xreconstruct[self.imask,self.jmask]
-		self.xxTsum = np.sum(np.square(self.X2unmasked))# can;t be pre-calculate in the missing data version :(
+		self.X2reconstruct[self.imask,self.jmask] = np.dot(self.m_Z,self.W.T)[self.imask,self.jmask]
+		self.xxTsum = np.sum(np.square(self.X2reconstruct))# can;t be pre-calculate in the missing data version :(
 		
 	def M_step(self):
 		""" This should handle missing data - needs testing (TODO)"""
 		zzT = np.dot(self.m_Z.T,self.m_Z) + np.sum(self.S_Z,0)
 		#self.W = np.dot(np.dot(self.X2.T,self.m_Z),np.linalg.inv(zzT))
 		zzT_chol = linalg.cholesky(zzT)
-		self.W = linalg.cho_solve((zzT_chol,0),np.dot(self.m_Z.T,self.X2unmasked)).T
+		self.W = linalg.cho_solve((zzT_chol,0),np.dot(self.m_Z.T,self.X2reconstruct)).T
 		WTW = np.dot(self.W.T,self.W)
-		self.sigma2 = self.xxTsum - 2*np.sum(np.dot(self.m_Z,self.W.T)*self.X2unmasked) + np.trace(np.dot(zzT,WTW))
+		self.sigma2 = self.xxTsum - 2*np.sum(np.dot(self.m_Z,self.W.T)*self.X2reconstruct) + np.trace(np.dot(zzT,WTW))
 		self.sigma2 /= self.N*self.d
 
 if __name__=='__main__':
 	q=5#latent dimensions
 	d=15# observed dimensions
 	N=500
-	missing_pc = 50 # percentage of the data points to be 'missing'
-	truesigma = .02
-	niters = 30
+	missing_pc = 100 # percentage of the data points to be 'missing'
+	truesigma = .002
+	niters = 300
 	phases = np.random.rand(1,q)*2*np.pi
 	frequencies = np.random.randn(1,q)*2
 	latents = np.sin(np.linspace(0,12,N).reshape(N,1)*frequencies-phases)
 	trueW = np.random.randn(d,q)
 	observed = np.dot(latents,trueW.T) + np.random.randn(N,d)*truesigma
+	
+	#PCA without missing values
 	a = PCA_EM(observed,q)
 	a.learn(niters)
 	
@@ -182,7 +183,7 @@ if __name__=='__main__':
 	pylab.title('Observed values')
 	pylab.subplot(2,1,2)
 	pylab.plot(observed2,linewidth=2,marker='.')
-	pylab.plot(b.X2unmasked + b.mu.T)
+	pylab.plot(b.Xreconstruct)
 
 	pylab.show()
 
