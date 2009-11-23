19a3043411663ee4c8aa9d4753132bbdca550f8c
James Hensman <james.hensman@gmail.com>
2009-11-23 10:43:24 +0100

    modified:   PCA_EM.py - started work on missing data class. Initialisation complete, need to implement E and M steps for missing data

--- PCA_EM.py
+++ PCA_EM.py
@@ -72,10 +72,47 @@ class PCA_EM:
 		self.W = linalg.cho_solve((zzT_chol,0),np.dot(self.m_Z.T,self.X2)).T
 		WTW = np.dot(self.W.T,self.W)
 		self.sigma2 = self.xxTsum - 2*np.sum(np.dot(self.m_Z,self.W.T)*self.X2) + np.trace(np.dot(zzT,WTW))
-		#self.sigma2 = self.xxTsum - 2*ml.trace(self.m_Z*self.W.T*self.X2.T) + ml.trace(zzT*WTW)
-		#self.sigma2 = self.xxTsum + ml.sum([- 2*z*self.W.T*x.T + ml.trace((z.T*z + self.S_z)*WTW) for z,x in zip(self.m_Z, self.X2)])
 		self.sigma2 /= self.N*self.d
 		
+class PCA_EM_missing:
+	def __init__(self,data,target_dim):
+		"""Maximum likelihood PCA by the EM algorithm, allows for missing data.  uses a masked array to 'hide' the elements of X that are NaN"""
+		self.X = np.array(data)
+		self.Xmasked = np.ma.masked_array(X,np.ma.make_mask(np.isnan(self.X)))
+		self.N,self.d = self.X.shape
+		self.q = target_dim
+	def learn(self,niters):
+		self.mu = np.asarray(self.Xmasked.mean(0)).reshape(self.d,1)#ML solution for mu
+		self.X2masked = self.Xmasked - self.mu.T
+		self.xxTsum = np.sum(np.square(X2masked))#precalculate for speed
+		#initialise paramters:
+		self.W = np.random.randn(self.d,self.q)
+		self.sigma2 = 1.2
+		for i in range(niters):
+			#print self.sigma2
+			self.E_step()
+			self.M_step()
+
+	def E_step(self):
+		""" TODO: this should handle missing data"""
+		M = np.dot(self.W.T,self.W) + np.eye(self.q)*self.sigma2
+		#M_inv = np.linalg.inv(M)
+		#self.m_Z = np.dot(M_inv,np.dot(self.W.T,self.X2.T)).T
+		#self.S_z = M_inv*self.sigma2
+		M_chol = linalg.cholesky(M)
+		M_inv = linalg.cho_solve((M_chol,1),np.eye(self.q))
+		self.m_Z = linalg.cho_solve((M_chol,1),np.dot(self.W.T,self.X2.T)).T
+		self.S_z = M_inv*self.sigma2
+		
+	def M_step(self):
+		""" TODO: this should handle missing data"""
+		zzT = np.dot(self.m_Z.T,self.m_Z) + self.N*self.S_z
+		#self.W = np.dot(np.dot(self.X2.T,self.m_Z),np.linalg.inv(zzT))
+		zzT_chol = linalg.cholesky(zzT)
+		self.W = linalg.cho_solve((zzT_chol,0),np.dot(self.m_Z.T,self.X2)).T
+		WTW = np.dot(self.W.T,self.W)
+		self.sigma2 = self.xxTsum - 2*np.sum(np.dot(self.m_Z,self.W.T)*self.X2) + np.trace(np.dot(zzT,WTW))
+		self.sigma2 /= self.N*self.d
 
 if __name__=='__main__':
 	q=2
